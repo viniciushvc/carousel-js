871b326b0706f720326a1d8e0f7349ef9b6fc207
Vinicius Vicentini <viniciusvicentini@live.com>
2018-09-21 10:49:31 -0300

    created example

--- dist/js/carousel.js
+++ dist/js/carousel.js
@@ -1,1 +1,40 @@
-function showSlides(e){e>slides.length&&(slideIndex=1),e<1&&(slideIndex=slides.length);for(var s=0;s<slides.length;s++)slides[s].classList.remove("active");for(s=0;s<indicators.length;s++)indicators[s].className=indicators[s].className.replace(" active","");slides[slideIndex-1].classList.add("active"),indicators[slideIndex-1].className+=" active"}for(var slideIndex=1,indicators=document.getElementsByClassName("carousel-indicators-item"),slides=document.getElementsByClassName("carousel-item"),i=0;i<indicators.length;i++)indicators[i].onclick=function(){showSlides(slideIndex=i+1)};document.querySelector(".carousel-prev").onclick=function(){showSlides(slideIndex-=1)},document.querySelector(".carousel-next").onclick=function(){showSlides(slideIndex+=1)},showSlides(slideIndex);
+var slideIndex = 1;
+
+var indicators = document.getElementsByClassName('carousel-indicators-item');
+
+var slides = document.getElementsByClassName('carousel-item');
+
+for (var i = 0; i < indicators.length; i++) {
+	indicators[i].onclick = function () {
+		showSlides(slideIndex = i + 1);
+	};
+}
+
+document.querySelector('.carousel-prev').onclick = function () {
+	showSlides(slideIndex -= 1);
+};
+
+document.querySelector('.carousel-next').onclick = function () {
+	showSlides(slideIndex += 1);
+};
+
+function showSlides(n) {
+
+	if (n > slides.length)
+		slideIndex = 1;
+
+	if (n < 1)
+		slideIndex = slides.length;
+
+	for (var i = 0; i < slides.length; i++)
+		slides[i].classList.remove('active');
+
+	for (i = 0; i < indicators.length; i++)
+		indicators[i].className = indicators[i].className.replace(' active', '');
+
+	slides[slideIndex - 1].classList.add('active');
+
+	indicators[slideIndex - 1].className += ' active';
+}
+
+showSlides(slideIndex);
--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var autoprefixer = require('gulp-autoprefixer');
 var csso = require('gulp-csso');
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 
 // Browsers that you want to support
 const AUTOPREFIXER_BROWSERS = [
@@ -22,14 +23,23 @@ const AUTOPREFIXER_BROWSERS = [
 gulp.task('styles', function () {
 	return gulp.src('src/css/style.css')
 		.pipe(autoprefixer({ browsers: AUTOPREFIXER_BROWSERS }))
+		.pipe(gulp.dest('./dist/css'))
 		.pipe(csso())
+		.pipe(rename({
+			suffix: '.min'
+		}))
 		.pipe(gulp.dest('./dist/css'))
 });
 
 // JS
 gulp.task('scripts', function () {
 	return gulp.src('./src/js/carousel.js')
+		.pipe(gulp.dest('./dist/js'))
+		
 		.pipe(uglify())
+		.pipe(rename({
+			suffix: '.min'
+		}))
 		.pipe(gulp.dest('./dist/js'))
 });
 
